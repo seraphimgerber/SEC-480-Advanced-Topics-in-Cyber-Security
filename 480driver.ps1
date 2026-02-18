eedb62446d9b2acd2bc1e20fe2ee95b70c57daad
seraphim <113275109+seraphimgerber@users.noreply.github.com>
2026-02-17 23:18:19 -0500

    Update 480driver.ps1

--- 480driver.ps1
+++ 480driver.ps1
@@ -1,2 +1,22 @@
 Import-Module '/home/xubuntu/SEC-480-Advanced-Topics-in-Cyber-Security/modules/480-utils' -Force
-cloner -config_path "/home/xubuntu/SEC-480-Advanced-Topics-in-Cyber-Security/480.json"
+$vmName = Read-Host "Enter the name of the VM to clone"
+$snapshotName = Read-Host "Enter the snapshot name [Base]"
+if ([string]::IsNullOrWhiteSpace($snapshotName)) { $snapshotName = "Base" }
+$newName = Read-Host "Enter the name for the new VM"
+$esxi = Read-Host "ESXi host [192.168.3.210]"
+if ([string]::IsNullOrWhiteSpace($esxi)) { $esxi = "192.168.3.210" }
+$datastore = Read-Host "Datastore [datastore2-super10]"
+if ([string]::IsNullOrWhiteSpace($datastore)) { $datastore = "datastore2-super10" }
+$network = Read-Host "Network [480-WAN]"
+if ([string]::IsNullOrWhiteSpace($network)) { $network = "480-WAN" }
+
+$clone_type = Read-Host "Clone type? 'F'ull or 'L'inked"
+while ($clone_type -ne 'F' -and $clone_type -ne 'L') {
+    $clone_type = Read-Host "Invalid. Enter 'F' or 'L'"
+}
+
+if ($clone_type -eq 'L') {
+    New-LinkedClone -vmName $vmName -snapshotName $snapshotName -newName $newName -esxi $esxi -datastore $datastore -network $network
+} elseif ($clone_type -eq 'F') {
+    New-FullClone -vmName $vmName -snapshotName $snapshotName -newName $newName -esxi $esxi -datastore $datastore -network $network
+}
